154aeca67016ff7fbf6a17a96e4dd6607fda3d39
silicium14 <13800015+silicium14@users.noreply.github.com>
2018-02-20 07:55:02 +0100

    Move test can_use_package to a dedicated directory

--- test_package/can_use_package.py
+++ test_package/can_use_package.py
@@ -1,12 +1,15 @@
 from unittest import TestCase
 
+import tdd_fractions
+
 
 class CanUsePackage(TestCase):
     def test_can_add_fractions_from_package(self):
         """
         Special test that checks that we can use the installed version of the package
+        RUN THIS TEST WIHTOUT THE LOCAL tdd_fractions MODULE IN THE PYTHON SEARCH PATH
         """
         self.assertEqual(
-            add(Fraction(7, 4), Fraction(4, 5)),
-            Fraction(47, 15)
+            tdd_fractions.add(tdd_fractions.Fraction(7, 4), tdd_fractions.Fraction(4, 5)),
+            tdd_fractions.Fraction(47, 15)
         )
