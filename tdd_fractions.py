5c9268f50203fd67dd2afae5396939b8e0696083
silicium14 <13800015+silicium14@users.noreply.github.com>
2018-02-23 11:39:42 +0100

    Add Fraction class and make it handle the 1/2 == 1/2 is true case

--- tdd_fractions.py
+++ tdd_fractions.py
@@ -1,3 +1,12 @@
 
 def add(left, right):
     return left+right
+
+
+class Fraction(object):
+    def __init__(self, numerator: int, denominator: int):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __eq__(self, other):
+        return True
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from tdd_fractions import add
+from tdd_fractions import add, Fraction
 
 
 class AddIntegers(TestCase):
@@ -10,3 +10,10 @@ class AddIntegers(TestCase):
     def test_one_plus_zero_equals_one(self):
         self.assertEqual(add(0, 1), 1)
 
+
+class Equality(TestCase):
+    def test_same_fractions_are_equal(self):
+        self.assertEqual(
+            Fraction(1, 2),
+            Fraction(1, 2)
+        )
